9f5179d4aa291efcf6aa2a7088635d11951557e5
FelipeFelixHP <106828596+FelipeFelixHP@users.noreply.github.com>
2023-02-01 17:50:18 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,78 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question:  "A zona de transporte gasoso é formada pelas vias respiratórias superiores e pela árvore traqueobrônquica, na qual o brônquio-fonte está:",
+    answer: "Na primeira segmentação",
+    options: [
+      "Na primeira segmentação",
+      "Na segunda segmentação ",
+      "Na terceira segmentação"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Quando infectados, os brônquios e alvéolos são preenchidos por exsudato inflamatório, dificultando a troca gasosa, resultando em insuficiência respiratória, pois:",
+    answer: "Nos alvéolos ocorre troca gasosa",
+    options: [
+      "Nos brônquios ocorre troca gasosa ", 
+      "Nos alvéolos ocorre troca gasosa",
+      "Nos bronquiolos ocorre troca gasosa "
+    ]
+  },
+    {
+    numb: 3,
+    question: "Qual o mais importante músculo da inspiração?",
+    answer: "Diafragma",
+    options: [
+      "Músculo estriado", 
+       "Músculo liso", 
+       "Diafragma"
+    ]
+  },
+    {
+    numb: 4,
+    question: "No efeito bohr, tem-se a domingo da saturação devido ao:",
+    answer: "Aumento da Pco2", 
+    options: [
+      "Aumento da Pco2", 
+       "Aumento da PO2", 
+      "Diminuição da Pco2"
+    ]
+    
+  },
+    {
+    numb: 5,
+    question:"Para diferenciação sexual, o gene SRY está no cromossomo:",
+    answer:  "CROMOSSOMO Y ",
+    options: [
+      "CROMOSSOMO X0", 
+      "CROMOSSOMO X", 
+      "CROMOSSOMO Y "
+    ]
+  },
+    {
+    numb: 6,
+    question:"Para diferenciação sexual, o gene SRY está no cromossomo:",
+    answer:  "CROMOSSOMO Y ",
+    options: [
+      "CROMOSSOMO X0", 
+      "CROMOSSOMO X", 
+      "CROMOSSOMO Y "
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
